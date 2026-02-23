5bb1d911fb8d4902b2d110c94c4e1f29f3d27e2f
gislih24 <gislih24@ru.is>
2026-02-23 14:23:43 +0000

    Use formatter on code

--- AST.cpp
+++ AST.cpp
@@ -4,14 +4,12 @@
 #include <stdexcept>
 #include <vector>
 
-
-
-Node::Node(std::int64_t v)
-    : type(NodeType::Number), value(v) {}
+Node::Node(std::int64_t v) : type(NodeType::Number), value(v) {}
 
 Node::Node(NodeType t, std::unique_ptr<Node> l, std::unique_ptr<Node> r)
     : type(t), value(0), left(std::move(l)), right(std::move(r)) {}
-//tokenizer
+
+// Tokenizer
 std::vector<Token> tokenize(const std::string& s) {
     std::vector<Token> tokens;
 
@@ -22,7 +20,8 @@ std::vector<Token> tokenize(const std::string& s) {
         }
         if (std::isdigit(static_cast<unsigned char>(s[i]))) {
             std::int64_t value = 0;
-            while (i < s.size() && std::isdigit(static_cast<unsigned char>(s[i]))) {
+            while (i < s.size() &&
+                   std::isdigit(static_cast<unsigned char>(s[i]))) {
                 value = value * 10 + (s[i] - '0');
                 ++i;
             }
@@ -30,34 +29,34 @@ std::vector<Token> tokenize(const std::string& s) {
             continue;
         }
         switch (s[i]) {
-            case '+':
-                tokens.push_back({TokenType::Plus, 0});
-                break;
-            case '-':
-                tokens.push_back({TokenType::Minus, 0});
-                break;
-            case '*':
-                tokens.push_back({TokenType::Mult, 0});
-                break;
-            case '(':
-                tokens.push_back({TokenType::LParen, 0});
-                break;
-            case ')':
-                tokens.push_back({TokenType::RParen, 0});
-                break;
-            default:
-                throw std::runtime_error("invalid character");
+        case '+':
+            tokens.push_back({TokenType::Plus, 0});
+            break;
+        case '-':
+            tokens.push_back({TokenType::Minus, 0});
+            break;
+        case '*':
+            tokens.push_back({TokenType::Mult, 0});
+            break;
+        case '(':
+            tokens.push_back({TokenType::LParen, 0});
+            break;
+        case ')':
+            tokens.push_back({TokenType::RParen, 0});
+            break;
+        default:
+            throw std::runtime_error("invalid character");
         }
         ++i;
     }
     tokens.push_back({TokenType::End, 0});
     return tokens;
 }
-    
+
 int main() {
     std::string input;
     std::getline(std::cin, input);
-    auto tokens = tokenize(input);
+    auto tokens = ::tokenize(input);
 
     for (const auto& t : tokens) {
         if (t.type == TokenType::Number)
--- AST.h
+++ AST.h
@@ -1,31 +1,19 @@
 #pragma once
 #include <memory>
 
-enum class NodeType {
-    Number, Add, Sub, Mult
-};
+enum class NodeType { Number, Add, Sub, Mult };
 
 struct Node {
     NodeType type;
-    std::int64_t value; //lets allow large integers cause why not
+    std::int64_t value; // lets allow large integers cause why not
     std::unique_ptr<Node> left;
     std::unique_ptr<Node> right;
 
     Node(std::int64_t v);
-    Node(NodeType t,
-        std::unique_ptr<Node> l,
-        std::unique_ptr<Node> r);
+    Node(NodeType t, std::unique_ptr<Node> l, std::unique_ptr<Node> r);
 };
 
-enum class TokenType {
-    Number,
-    Plus,
-    Minus,
-    Mult,
-    LParen,
-    RParen,
-    End
-};
+enum class TokenType { Number, Plus, Minus, Mult, LParen, RParen, End };
 
 struct Token {
     TokenType type;
