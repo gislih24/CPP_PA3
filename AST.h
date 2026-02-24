b0a6fbbaa1640b4f9cc5e4b5f228f6e7d86f6e1d
gislih24 <gislih24@ru.is>
2026-02-24 11:59:09 +0000

    Implement `get_value()` method in `Node` struct

--- AST.h
+++ AST.h
@@ -9,6 +9,26 @@ struct Node {
     std::unique_ptr<Node> left;
     std::unique_ptr<Node> right;
 
+    std::int64_t get_value() {
+        if (type == NodeType::Number) {
+            return value;
+        } else if (type == NodeType::Add) {
+            value = left->get_value() + right->get_value();
+        } else if (type == NodeType::Sub) {
+            value = left->get_value() - right->get_value();
+        } else if (type == NodeType::Mult) {
+            value = left->get_value() * right->get_value();
+        } else {
+            return 0;
+        }
+        type = NodeType::Number;
+        // Since we're using unique_ptr, this will automatically delete the
+        // left node, since nothing is pointing to it.
+        left = nullptr;
+        right = nullptr;
+        return value;
+    }
+
     Node(std::int64_t v);
     Node(NodeType t, std::unique_ptr<Node> l, std::unique_ptr<Node> r);
 };
